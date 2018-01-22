1362e8ba6c2cfdaf32dd437d6272990ec528642b
jboone <jboone@pariserderm.com>
2018-01-22 07:38:57 -0500

    update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -12,18 +12,19 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export const appRoutes: Routes = [
- { path: 'home', component: HomeComponent },
- { path: '', runGuardsAndResolvers: 'always', canActivate: [AuthGuard] , children: [
-  { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver} },
-  { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver } },
-   {
-     path: 'member/edit',
-     component: MemberEditComponent,
-     resolve: { user: MemberEditResolver },
-     canDeactivate: [PreventUnsavedChanges]
-   },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'lists', component: ListsComponent },
- ]},
- { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '', runGuardsAndResolvers: 'always', canActivate: [AuthGuard] , children: [
+    { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver} },
+    { path: 'members/:id', component: MemberDetailComponent, resolve: { user: MemberDetailResolver } },
+    {
+      path: 'member/edit',
+      component: MemberEditComponent,
+      resolve: { user: MemberEditResolver },
+      canDeactivate: [PreventUnsavedChanges]
+    },
+    { path: 'messages', component: MessagesComponent },
+    { path: 'lists', component: ListsComponent },
+  ]},
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
